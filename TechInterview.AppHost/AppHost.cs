fb6951b5d4aa41278c5e166fc069946852e72f88
Massimo Fornari <massimo.fornari@fortech.it>
2026-03-11 18:03:15 +0100

    fix(all): pulizia

--- TechInterview.AppHost/AppHost.cs
+++ TechInterview.AppHost/AppHost.cs
@@ -10,14 +10,9 @@ var airports = builder.AddProject<Projects.AirportsService>("airportsservice").W
 
 var flights = builder.AddProject<Projects.FlightsService>("flightsservice");
 
-var apiService = builder.AddProject<Projects.TechInterview_ApiService>("apiservice")
-    .WithHttpHealthCheck("/health");
-
 builder.AddProject<Projects.TechInterview_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithHttpHealthCheck("/health")
-    .WithReference(apiService)
-    .WaitFor(apiService)
     .WithReference(flights)
     .WaitFor(flights)
     .WithReference(airports)
